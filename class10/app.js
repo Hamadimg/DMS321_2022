9a91f1e1760c744ad96f6602a0d9a38da58916c7
Dave Pape <depape@buffalo.edu>
2022-03-04 19:16:48 -0800

    rearrange; comment-out older examples

--- class10/app.js
+++ class10/app.js
@@ -5,22 +5,30 @@ var urlencodedParser=bodyParser.urlencoded({extended:false});
 
 const makeHTMLPage = require('./makehtml.js').makeHTMLPage;
 
-const rps = require('./rps.js');
-const miniblog = require('./miniblog.js');
-const mymongo = require('./mymongo.js');
-
-
-
 function homepage(req, res) {
     res.set('Cache-Control','no-store');
     res.send(makeHTMLPage('<p>Hello World, from express</p>'));
     }
-
-
 app.get('/', homepage);
+
+
+/* Rock-Paper-Scissors */
+/*
+const rps = require('./rps.js');
+app.get('/rps/:choice', rps.RPSChoice);
+*/
+/* Mini-blog */
+/*
+const miniblog = require('./miniblog.js');
 app.get('/blog', miniblog.Blog);
 app.post('/blogpost', urlencodedParser, miniblog.BlogPost);
-app.get('/rps/:choice', rps.RPSChoice);
+*/
+
+
+/* MongoDB example (film title search) */
+const mymongo = require('./mymongo.js');
 app.get('/mongo/:title', mymongo.search);
+
+
 
 var server = app.listen(8081, function () {});
