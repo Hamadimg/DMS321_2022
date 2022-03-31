ee19ecdb8e48bc75f960446214ce7400e8cebaba
Dave Pape <depape@buffalo.edu>
2022-03-31 05:02:10 -0700

    nodemailer demo

---
+++ class16/sendmail.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+
+let toEmail = 'depape@buffalo.edu';
+let fromEmail = process.env.SOCIAL_EMAIL_ADDRESS;
+
+let transporter = nodemailer.createTransport({
+    host: process.env.SOCIAL_EMAIL_SERVER,
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    tls: {  rejectUnauthorized: false },
+    auth: {
+        user: process.env.SOCIAL_EMAIL_ADDRESS,
+        pass: process.env.SOCIAL_EMAIL_PASSWORD
+        }
+    });
+
+let mailOptions = {
+    from: `DMS 321 demo <${fromEmail}>`,
+    to: toEmail,
+    subject: 'nodejs test mail',
+    };
+
+mailOptions.text = `Hello, this is an email sent by nodejs.
+It was sent from the website ubdms321.xyz.
+Your lucky number today is 3.
+Goodbye.`
+
+transporter.sendMail(mailOptions, function (err,info) {
+        if (err)
+            console.log(err);
+        else
+            console.log(`mail sent to ${toEmail}`);
+        });
+
+
